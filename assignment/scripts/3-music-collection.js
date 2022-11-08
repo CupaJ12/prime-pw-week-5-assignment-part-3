d881625344c0da8ff803f2f149a2645c142d6576
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2022-11-08 13:11:35 -0500

    almost done with it

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,47 @@
 console.log('***** Music Collection *****')
+
+let collection = []
+
+function AddToCollection(title, artist, yearPublished) {
+    this.title = title;
+    this.artist = artist;
+    this.yearPublished = yearPublished;
+    collection.push(this);
+    return this;
+}
+let album = new AddToCollection('Thriller','Michael Jackson',1982);
+console.log(album);
+console.log(collection);
+let album2 = new AddToCollection('Nevermind','Nirvana',1991);
+let album3 = new AddToCollection('Rumours','Fleetwood Mac',1977);
+let album4 = new AddToCollection('Appetite for Destruction','Guns N Roses',1987);
+let album5 = new AddToCollection('Hysteria','Def Leopard',1987);
+
+console.log(album)
+console.log(album2)
+console.log(album3)
+console.log(album4)
+console.log(album5)
+console.log(collection)
+
+function showCollection(array){
+    console.log(array.length)
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(array[index])
+    }
+}
+showCollection(collection)
+
+function findByArtist(pam1){
+    let artistStuff = []
+   
+    for (let index = 0; index < collection.length; index++) {
+        const element = collection[index];
+        if pam1 = artist {
+            artistStuff.push()
+        }
+
+    }
+}
+findByArtist('Michael Jackson')
