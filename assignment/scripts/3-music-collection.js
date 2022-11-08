280916cd1a51a900777568c273f39e02739f45a4
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2022-11-08 16:12:24 -0500

    Finished!

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,45 +3,68 @@ console.log('***** Music Collection *****')
 let collection = []
 
 function AddToCollection(title, artist, yearPublished) {
-    this.title = title;
-    this.artist = artist;
-    this.yearPublished = yearPublished;
-    collection.push(this);
-    return this;
+    record = {title, artist, yearPublished};
+    this.title = collection.title;
+    this.artist = collection.artist;
+    this.yearPublished = collection.yearPublished;
+    collection.push({record});
+    return record;
 }
-let album = new AddToCollection('Thriller','Michael Jackson',1982);
-console.log(album);
-console.log(collection);
-let album2 = new AddToCollection('Nevermind','Nirvana',1991);
-let album3 = new AddToCollection('Rumours','Fleetwood Mac',1977);
-let album4 = new AddToCollection('Appetite for Destruction','Guns N Roses',1987);
-let album5 = new AddToCollection('Hysteria','Def Leopard',1987);
+// let album = new AddToCollection('Thriller','Michael Jackson',1982);
+// console.log(album);
+// console.log(collection);
+// let album2 = new AddToCollection('Nevermind','Nirvana',1991);
+// let album3 = new AddToCollection('Rumours','Fleetwood Mac',1977);
+// let album4 = new AddToCollection('Appetite for Destruction','Guns N Roses',1987);
+// let album5 = new AddToCollection('Hysteria','Def Leopard',1987);
 
-console.log(album)
-console.log(album2)
-console.log(album3)
-console.log(album4)
-console.log(album5)
+
+
+AddToCollection('Thriller','Michael Jackson','1982');
+AddToCollection('Nevermind','Nirvana','1991');
+AddToCollection('Rumours','Fleetwood Mac','1977');
+AddToCollection('Appetite for Destruction','Guns N Roses','1987');
+AddToCollection('Hysteria','Def Leopard','1987');
+AddToCollection('Bobby','Michael Jackson','1983');
+
+
+
+// console.log(album)
+// console.log(album2)
+// console.log(album3)
+// console.log(album4)
+// console.log(album5)
 console.log(collection)
 
-function showCollection(array){
-    console.log(array.length)
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        console.log(array[index])
+function showCollection(searchString){
+    console.log(collection.length)
+    let selectedCollection = []
+    for (let index = 0; index < collection.length; index++) {
+        if (collection[index].record.artist === searchString) {
+            selectedCollection.push(collection[index].record);
+        }
+        if (collection[index].record.title === searchString) {
+            selectedCollection.push(collection[index].record);
+        }
+        if (collection[index].record.yearPublished === searchString) {
+            selectedCollection.push(collection[index].record);
+        }
+        
     }
+    return selectedCollection
 }
 showCollection(collection)
+console.log(showCollection('Michael Jackson'));
 
 function findByArtist(pam1){
     let artistStuff = []
    
     for (let index = 0; index < collection.length; index++) {
-        const element = collection[index];
-        if pam1 = artist {
-            artistStuff.push()
+        // const element = collection[index];
+        if (collection[index].record.artist === pam1) {
+            artistStuff.push(collection[index].record);
         }
-
     }
+    return artistStuff;
 }
-findByArtist('Michael Jackson')
+console.log(findByArtist('Michael Jackson'));
